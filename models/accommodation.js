d38cf32bb1cfd551ee84c3091f8b98654ab72b12
sonic11829 <sethneal15@ymail.com>
2022-11-15 20:43:35 -0500

    added accommodations to accommodation.js and corrected spelling errors

--- models/accommodation.js
+++ models/accommodation.js
@@ -1,9 +1,16 @@
 const mongoose = require('mongoose')
 
-const accommadationSchema = new mongoose. Schema({
-    word: { type: String, required: true },
+const accommodationSchema = new mongoose.Schema({
+    auditory: { type: String, required: true },
+    behavioral: { type: String, required: true },
+    cognitive: { type: String, required: true },
+    speech: { type: String, required: true },
+    time: { type: String, required: true },
+    vision: { type: String, required: true },
+    flexible: { type: String, required: true },
+    physical: { type: String, required: true }
 });
 
-const Accomodation = mongoose.model('Accommodation', accommadationSchema)
+const Accommodation = mongoose.model('Accommodation', accommodationSchema)
 
-module.exports = Accomodation
+module.exports = Accommodation
