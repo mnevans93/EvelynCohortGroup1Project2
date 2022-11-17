b0a4102abf5ce4c3a2488fd60bbfb71c20f6dfc4
moris96 <moriskhoudari@gmail.com>
2022-11-16 20:37:24 -0500

    updated search query

--- models/search.js
+++
@@ -1,7 +0,0 @@
-const search = [
-    { id: 1, name: 'Kaiba', age: 16, city: 'Battle City' },
-    { id: 2, name: 'Pegasus', age: 40, city: 'Duel Kingdom' },
-    { id: 3, name: 'Joey Wheeler', age: 16, city: 'Battle City' },
-]
-
-module.exports = search 
--- server.js
+++ server.js
@@ -1,37 +1,52 @@
 const express = require('express')
-const search = require('./models/search')
+const { Mongoose } = require('mongoose')
+
 const PORT = process.env.PORT || 3000
 
-
-const app = express();
-
+const app = express()
 
 
 
-
-app.use('/search', (req, res, next) => {
-    const filters = req.query;
-    const filteredSearch = search.filter(user => {
-      let isValid = true;
-      for (key in filters) {
-        console.log(key, user[key], filters[key]);
-        isValid = isValid && user[key] == filters[key];
-      }
-      return isValid;
-    });
-    res.send(filteredSearch);
-});
 
 app.get('/', function (req, res) {
-  res.send(`<h1>Hello Search</h1>
-  <form id="form" role="search">
-  <input type="search" id="query" name="q"
+  res.send(`<h1>Search For Job Postings</h1>
+  <form id="form" action="/search/" role="search">
+  <input type="search" id="query" name="accomodationsId"
    placeholder="Search..."
    aria-label="Search through site content">
   <button>Search</button>
 </form>`)
 })
 
+app.get('/search', (req, res) => {
+  // assume there is a filter and a value on req.query or theres an error
+  if(req.query.filter && req.query.value){
+     search.where(req.query.filter).equals(req.query.value).exec((err, results) => {
+       if(err){
+           res.status(400).json({ msg: err})
+        } else {
+         res.json(results)
+        }
+    })
+  } else (
+   res.status(400).json({ msg: 'malformed query, must have filter and value'})
+ )
+ })
+
+//only thing we need to search is accomodations. should show up as a list of booleans from the database 
+
+app.use('/search', (req, res, next) => {
+    const filters = req.body.accomodationsId
+    const filteredSearch = search.filter(user => {
+      let isValid = true
+      for (key in filters) {
+        console.log(key, user[key], filters[key])
+        isValid = isValid && user[key] == filters[key]
+      }
+      return isValid
+    })
+    res.send(filteredSearch)
+})
 
 
 
