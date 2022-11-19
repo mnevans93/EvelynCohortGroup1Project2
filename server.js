092f4c729042a07881bac870af8bd9c826f62d0b
Matthias Nevans <mnevans93@gmail.com>
2022-11-19 10:34:30 -0500

    Added app.use json line in server.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 app.use((req, res, next) => {
   res.locals.data = {}
   next()
