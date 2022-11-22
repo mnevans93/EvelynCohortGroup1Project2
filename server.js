e15c89b31c6c20c2356c274a4aa55ee7d673ddda
Matthias Nevans <mnevans93@gmail.com>
2022-11-22 18:37:25 -0500

    re-added port definition line that was unintentionally removed during merge conflict resolution

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express')
 const methodOverride = require('method-override')
 const database = require('./models/database')
 const app = express()
+const PORT = process.env.PORT || 3000
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
